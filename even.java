0c00979ffbb672c30db398e02d3bcea3ee6d7a06
Rajalakshmivenkat <rajivenkat.ece@gmail.com>
2016-07-10 11:25:56 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Even
+{
+public static void main(String [] args)
+{
+int i;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number"+i);
+i=sc.nextInt();
+if(i%2==0)
+{
+System.out.println("the number is even");
+}
+else
+{
+System.out.println("The number is odd");
+}
+}
+}
